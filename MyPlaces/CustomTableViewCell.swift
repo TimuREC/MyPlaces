a4e53aae8da51e1f1227f1172a65d5729b2ad24a
Timur Begishev <rpsi-mono@yandex.ru>
2021-01-18 17:42:36 +0300

    Created custom class for cells, changed font for Navigation Controller titles

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Timur Begishev on 18.01.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var imageOfPlace: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var locationLabel: UILabel!
+	@IBOutlet weak var typeLabel: UILabel!
+
+}
--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -23,14 +23,12 @@ class MainViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 		let restaurant = restaurants[indexPath.row]
 
-		cell.textLabel?.text = restaurant
-		cell.imageView?.image = UIImage(named: restaurant)
-		cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-		cell.imageView?.clipsToBounds = true
-		cell.imageView?.contentMode = .scaleAspectFit
+		cell.nameLabel.text = restaurant
+		cell.imageOfPlace.image = UIImage(named: restaurant)
+		cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
 
         return cell
     }
