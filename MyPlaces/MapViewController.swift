3612f3cac8e2018de451433eade4be63a1dba2d8
Timur Begishev <rpsi-mono@yandex.ru>
2021-01-25 18:40:29 +0300

    App prepared to work with MapKit

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Timur Begishev on 25.01.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+	@IBAction func closeViewController() {
+		dismiss(animated: true, completion: nil)
+	}
+	
+}
